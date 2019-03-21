b8b4e04b775a3f98c743eb23bfb81e529fd5e945
Arpit Jain <arpit.jain02@sap.com>
2019-03-22 03:56:23 +0530

    First commit

--- GithubIssues/views.py
+++ GithubIssues/views.py
@@ -36,20 +36,16 @@ def home(request):
             # call github api from requests.get() function -->
             lo_issues_data = requests.get(lv_api_main + lv_api_pagination)
             if lo_issues_data.status_code == 403:
-                messages.error(request,
-                               "Maximum requests within 1 hour to Github API reached. Please try after 1 hour. MORE DETAILES: https://developer.github.com/v3/#rate-limiting")
+                messages.error(request,"Maximum requests within 1 hour to Github API reached. Please try after 1 hour. MORE DETAILES: https://developer.github.com/v3/#rate-limiting")
             elif lo_issues_data.status_code == 422:
                 messages.error(request, "Please enter valid github repository link")
             else:
                 # convert it into json -->get "total_count" value from json
                 lv_total_issue_count = lo_issues_data.json()["total_count"]
-                lv_last_24hrs_issues = \
-                requests.get(lv_api_main + lv_api_created_tag + Data.getOldTimeStamp(1) + lv_api_pagination).json()[
-                    "total_count"]
+                lv_last_24hrs_issues = requests.get(lv_api_main + lv_api_created_tag + Data.getOldTimeStamp(1) + lv_api_pagination).json()["total_count"]
+
                 # subtracting number of issues occured in last 24 hours from no. of issues in last 7 days
-                lv_last_24hrs_to_7days_issues = \
-                requests.get(lv_api_main + lv_api_created_tag + Data.getOldTimeStamp(7) + lv_api_pagination).json()[
-                    "total_count"] - lv_last_24hrs_issues
+                lv_last_24hrs_to_7days_issues = requests.get(lv_api_main + lv_api_created_tag + Data.getOldTimeStamp(7) + lv_api_pagination).json()["total_count"] - lv_last_24hrs_issues
 
                 # # getting remaining issues
                 lv_remaining_issues = lv_total_issue_count - lv_last_24hrs_to_7days_issues - lv_last_24hrs_issues
